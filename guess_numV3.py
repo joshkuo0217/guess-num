1ef74f349518e6b49aa46e5d29924c9787c501b5
Chien Yu Kuo <josh820217@gmail.com>
2021-03-14 22:05:31 +0800

    modified version

--- guess_numV3.py
+++ guess_numV3.py
@@ -1,16 +1,16 @@
 import random
 start = input('請決定隨機數字開始值: ')
-end = input('請決定隨機數字結束值:')
+end = input('請決定隨機數字結束值: ')
 start = int(start)
-end = int(start)
+end = int(end)
 r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1 # count += 1 為快寫法
-	r_input = input('請輸入一個數字: ')
+	r_input = input('請輸入一個介於{}與{}之間的數字: '.format(start, end))
 	r_input = int(r_input)
 	if r == r_input:
-		print('恭喜!你答對了,這是你猜的第', count,'次呦')
+		print('恭喜!你答對了,這是你猜的第', count, '次呦')
 		break
 	elif r != r_input:
 		if r_input < r:
